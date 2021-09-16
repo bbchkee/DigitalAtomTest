4d22263e07d76b1601563011e9c8d76e6ba55d26
bbchkee <domifa@rambler.ru>
2021-09-16 18:04:12 +0800

    bugfix. correct calculation of mean cost

--- query.sql
+++ query.sql
@@ -1,15 +1,21 @@
 SELECT 
     station_name AS station_name, 
-    SUM(tarif.hour_cost)/COUNT(tarif) AS mean_cost_per_hour,
+    (SELECT AVG(tarif.hour_cost)
+        FROM tarif WHERE tarif.station_id = station.id
+        GROUP BY station.id
+        ORDER BY station.id)
+        AS mean_cost_per_hour,
     SUM(consumption.volume_per_hour*tarif.hour_cost) AS total_cost
 FROM        
     station JOIN tarif 
-        ON tarif.station_id = station.id
-    LEFT OUTER JOIN consumption
-        ON consumption.tarif_name = tarif.tarif_name AND
-           consumption.hour >= tarif.tarif_begin AND 
-           consumption.hour < tarif.tarif_end
-GROUP BY 
+        ON tarif.station_id = station.id 
+    JOIN consumption
+        ON  consumption.tarif_name = tarif.tarif_name 
+        AND consumption.hour >= tarif.tarif_begin 
+        AND consumption.hour < tarif.tarif_end
+GROUP BY
     station.id
-ORDER BY 
+ORDER BY
     station.id
+;
+
